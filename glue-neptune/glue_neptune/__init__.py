cd9c248088e8a05e8e8cd5e0c99818792d7c4696
SuperStar0000000 <jacobmirelez34@gmail.com>
2024-08-07 09:27:10 -0500

    Update __init__.py

--- glue-neptune/glue_neptune/__init__.py
+++ glue-neptune/glue_neptune/__init__.py
@@ -11,5 +11,3 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
-
-__author__ = 'Ian Robinson (ianrob@amazon.com)'
